48647902d4d9aa7bf93b0acd27cd8b48afb4e6d4
ptsans <kzone416@gmail.com>
2021-03-07 11:10:44 +0800

    feat(app): add paper component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
+  <v-paper />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import VPaper from './components/Paper'
 
 export default defineComponent({
   name: 'App',
   setup() {
   },
   components: {
+    VPaper
   }
 })
 </script>
 
 <style>
+body {
+  margin: 0;
+  background: #e9ecef;
+  font-size: 16px;
+}
 
-  body {
-    margin: 0;
-  }
-
-  #app {
-    font-family: 'Montserrat', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
+#app {
+  font-family: 'Montserrat', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: grid;
+  justify-content: center;
+  padding: 2rem 0;
+}
 </style>
