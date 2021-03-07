95bf8daadda9197661ba5a3ab109785f8001867f
ptsans <kzone416@gmail.com>
2021-03-07 11:10:34 +0800

    feat: add paper component

---
+++ src/components/Paper/VPaper.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="paper">
+
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'VPaper',
+  components: {
+  }
+})
+</script>
+
+<style scoped>
+.paper {
+  width: 210mm;
+  height: 296mm; /*Fix -1px page break*/
+  padding: 10mm;
+  background: #fff;
+  box-sizing: border-box;
+  display: grid;
+}
+</style>
