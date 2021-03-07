11b287e9ea2322f812874ce0b8a6833b01de868e
ptsans <kzone416@gmail.com>
2021-03-07 22:43:58 +0800

    feat: add chip component

---
+++ src/components/Paper/VChip.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="chip">
+    <slot name="icon" />
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'VChip'
+})
+</script>
+
+<style scoped>
+.chip {
+  display: flex;
+  place-items: center;
+  font-size: 0.9rem;
+  color: var(--secondary-text);
+  font-weight: 500;
+}
+.chip::v-deep(svg) {
+  width: 1rem;
+  margin-right: 0.2rem;
+  fill: var(--secondary-text);
+}
+</style>
