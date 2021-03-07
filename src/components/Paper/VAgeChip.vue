6fe7bb66b7f34666bc4320b9518c8ddda0fce871
ptsans <kzone416@gmail.com>
2021-03-07 22:44:12 +0800

    feat: add age chip component

---
+++ src/components/Paper/VAgeChip.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-chip>
+    <v-person-icon />
+    <slot></slot>
+  </v-chip>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import VChip from './VChip.vue'
+import VPersonIcon from '@/components/Icons/VPersonIcon.vue'
+
+export default defineComponent({
+  name: 'VAgeChip',
+  components: {
+    VPersonIcon,
+    VChip
+  }
+})
+</script>
