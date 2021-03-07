75c2dd4c4ceaf3b60370e09d2c4206cb5cda4925
ptsans <kzone416@gmail.com>
2021-03-07 17:56:51 +0800

    feat(paper): add header component

--- src/components/Paper/VPaper.vue
+++ src/components/Paper/VPaper.vue
@@ -1,15 +1,48 @@
 <template>
   <main class="paper">
-
+    <v-header>
+      <template #full-name>
+        {{header.full_name}}
+      </template>
+      <template #job-position>
+        {{header.job_position}}
+      </template>
+      <template #avatar>
+        <img :src="`${publicPath}${header.avatar}`" alt="avatar"/>
+      </template>
+    </v-header>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType, toRefs, ref } from 'vue'
+import VHeader from './VHeader.vue'
+
+export type THeader = {
+  full_name: string
+  job_position: string
+  avatar: string
+}
 
 export default defineComponent({
   name: 'VPaper',
   components: {
+    VHeader
+  },
+  props: {
+    header: {
+      type: Object as PropType<THeader>,
+      required: true
+    }
+  },
+  setup(props) {
+    const {
+      header
+    } = toRefs<{header: THeader}>(props)
+
+    const publicPath = ref(process.env.BASE_URL)
+
+    return { header, publicPath }
   }
 })
 </script>
@@ -22,5 +55,8 @@ export default defineComponent({
   background: #fff;
   box-sizing: border-box;
   display: grid;
+  grid-template-rows: 2fr 12fr;
+  grid-template-columns: 12fr 8fr;
+  border-radius: 5px;
 }
 </style>
