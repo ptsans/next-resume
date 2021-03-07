938f3ceefa655082db263768b4813927312ce616
ptsans <kzone416@gmail.com>
2021-03-07 22:44:24 +0800

    feat: add location chip component

---
+++ src/components/Paper/VLocationChip.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-chip>
+    <v-pin-icon />
+    <slot></slot>
+  </v-chip>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import VChip from './VChip.vue'
+import VPinIcon from '@/components/Icons/VPinIcon.vue'
+
+export default defineComponent({
+  name: 'VLocationChip',
+  components: {
+    VChip,
+    VPinIcon
+  }
+})
+</script>
