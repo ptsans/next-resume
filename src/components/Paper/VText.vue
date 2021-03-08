f016a6867be08984d1b024ae64b7c95981bdeae0
ptsans <kzone416@gmail.com>
2021-03-08 15:33:18 +0800

    feat: add card text component

---
+++ src/components/Paper/VText.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="text">
+    <slot />
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "VText",
+})
+</script>
+
+<style>
+.text {
+  margin: 1rem 0;
+  font-size: 1rem;
+  white-space: pre-wrap;
+}
+</style>
