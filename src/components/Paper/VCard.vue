6287af123a3853e80d83739473d3f11d94063a0d
ptsans <kzone416@gmail.com>
2021-03-08 15:33:54 +0800

    feat: add card component

---
+++ src/components/Paper/VCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <v-title>
+      {{ title }}
+    </v-title>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs } from 'vue'
+import VTitle from './VTitle.vue'
+
+export default defineComponent({
+  name: "VCard",
+  components: {
+    VTitle
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const { title } = toRefs<{title: string}>(props)
+    return { title }
+  }
+})
+</script>
