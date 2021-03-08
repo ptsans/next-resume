11659178cb21a256bd1ee3fb100ab9f0b890617c
ptsans <kzone416@gmail.com>
2021-03-08 15:29:06 +0800

    refactor: move format age func to utils folder

--- src/components/Paper/VHeader.vue
+++ src/components/Paper/VHeader.vue
@@ -27,6 +27,7 @@ import { defineComponent, reactive, ref, computed } from 'vue'
 import VLocationChip from './VLocationChip.vue'
 import VAgeChip from './VAgeChip.vue'
 import HeaderJson from '@/api/header.json'
+import { formatAge } from '@/utils/formatAge'
 
 type TDate = {
   year: number
@@ -66,25 +67,6 @@ export default defineComponent({
       }
 
       return formatAge(age)
-    }
-    const formatAge = (age: number): string => {
-      /*
-      * 0 лет
-      * 1 год
-      * 2-4 года
-      * 5-9 лет
-     */
-      const mod = age % 10
-      switch (true) {
-        case mod === 1:
-          return `${age} год`
-        case mod > 1 && mod < 5:
-          return `${age} года`
-        case mod === 0 || (mod > 4 && mod <= 9):
-          return `${age} лет`
-        default:
-          return `${age}`
-      }
     }
 
     const age = computed(() => dateToAge(header.born_date))
