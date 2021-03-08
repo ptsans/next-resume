1eba741964d62f346e0c4d553e1fb370d9254f39
ptsans <kzone416@gmail.com>
2021-03-08 23:43:30 +0800

    feat: add icons support for text component

--- src/components/Paper/VText.vue
+++ src/components/Paper/VText.vue
@@ -1,5 +1,6 @@
 <template>
   <p class="text">
+    <slot name="icon" />
     <slot />
   </p>
 </template>
@@ -12,10 +13,30 @@ export default defineComponent({
 })
 </script>
 
-<style>
+<style scoped>
 .text {
   margin: 1rem 0;
   font-size: 1rem;
-  white-space: pre-wrap;
+  white-space: pre-line;
+  display: flex;
+  place-items: center;
+  color: var(--primary-text);
+}
+.text::v-deep(svg) {
+  width: 1.3rem;
+  margin-left: 0.1rem;
+  margin-right: 0.5rem;
+  fill: var(--primary-main);
+}
+.text::v-deep(a) {
+  color: var(--primary-text);
+  text-decoration: none;
+}
+.text::v-deep(a:hover) {
+  text-decoration: underline;
+}
+.text_grey {
+  color: var(--secondary-text);
+  font-weight: 500;
 }
 </style>
