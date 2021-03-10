7265827555344b3a1eae58fc24da63a673746e58
ptsans <kzone416@gmail.com>
2021-03-10 14:25:09 +0800

    fix: fit paper content to height with paddings

--- src/components/Paper/VPaper.vue
+++ src/components/Paper/VPaper.vue
@@ -1,8 +1,10 @@
 <template>
   <main class="paper">
-    <v-header />
-    <v-content />
-    <v-sidebar />
+    <div class="paper__inner">
+      <v-header />
+      <v-content />
+      <v-sidebar />
+    </div>
   </main>
 </template>
 
@@ -24,15 +26,18 @@ export default defineComponent({
 
 <style scoped>
 .paper {
-  width: 210mm;
-  height: 296mm; /*Fix -1px page break*/
-  padding: 10mm;
+  --paper-padding: 10mm;
+  padding: var(--paper-padding);
   background: #fff;
-  box-sizing: border-box;
+  border-radius: 5px;
+}
+.paper__inner {
+  width: calc(210mm - (var(--paper-padding) * 2));
+  min-height: calc(297mm - (var(--paper-padding) * 2));
+  box-sizing: content-box;
   display: grid;
   grid-template-rows: auto 12fr;
   grid-template-columns: 13fr 8fr;
-  border-radius: 5px;
   row-gap: 1rem;
   column-gap: 4rem;
 }
